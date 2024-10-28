5c5945fb9773eb0734177c22d7c785529187a278
reza-foroutan <rezaforoutanjs@gmail.com>
2024-10-29 00:48:59 +0330

    digikala-ecommerce-v-00-00-04

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".mySwiperStorySlider", {
+  slidesPerView: 12.5,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// Initialize main home top slider Website
+var swiper = new Swiper(".mySwipermainHomeTopSlider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// End Initialize main home top slider Website
+// Initialize Amazing Carousel Website
+var swiper = new Swiper(".mySwiperAmazing", {
+  slidesPerView: 7.5,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// End Initialize Amazing Carousel Website
+var swiper = new Swiper(".mySwiper__hottest__last__hours", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// The hottest last few hours
+var swiper = new Swiper(".mySwiperhottestlasthours", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+});
+// End The hottest last few hours
+var swiper = new Swiper(".mySwipermostpopularbrands", {
+  slidesPerView: 9,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
