64dda889333dbcc007bdb571c4040400a27750e9
reza foroutan <143905099+reza-foroutan@users.noreply.github.com>
2025-03-23 12:45:22 +0330

    version-001

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Initialize Script stroy slider Website
+var swiper = new Swiper(".mySwiperstroyslider", {
+  slidesPerView: 12.5,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+// End Initialize Script stroy slider Website
