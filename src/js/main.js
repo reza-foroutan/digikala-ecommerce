3d6950b2cebd20d115129d8cd8d7cb3b9e1d4896
reza foroutan <143905099+reza-foroutan@users.noreply.github.com>
2025-03-24 12:02:36 +0330

    version-002

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,32 @@ var swiper = new Swiper(".mySwiperstroyslider", {
   },
 });
 // End Initialize Script stroy slider Website
+
+// Initialize main home top slider Website
+var swiper = new Swiper(".mySwipermainhometopslider", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+// End Initialize main home top slider Website
+
+// Initialize Amazing Website
+var swiper = new Swiper(".mySwiperAmazing", {
+  slidesPerView: 8,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+// End Initialize Amazing Website
